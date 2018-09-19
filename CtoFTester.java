f1d72b39da329c14867b15e3e165af4a74a1050a
Mohammad Jawwawd <mjawwad@stuy.edu>
2018-09-18 22:31:24 -0400

    Added compilable and runnable test examples of fxn

--- CtoFTester.java
+++ CtoFTester.java
@@ -3,5 +3,11 @@ public class CtoFTester {
     return (1.8 * c) + 32;
   }
   public static void main(String[] args) {
+    System.out.println(20.0 + " degrees Celsius is " +
+                celsiusToFahrenheit(20.0) + " degrees Fahrenheit (68)");
+    System.out.println(0.0 + " degrees Celsius is " +
+                celsiusToFahrenheit(0.0) + " degrees Fahrenheit (32)");
+    System.out.println(37.7777 + " degrees Celsius is " +
+                celsiusToFahrenheit(37.7777) + " degrees Fahrenheit (100)");
   }
 }
