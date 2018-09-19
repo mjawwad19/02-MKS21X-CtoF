741397e1591950818232b473f24a2074f208da58
Mohammad Jawwawd <mjawwad@stuy.edu>
2018-09-18 22:32:46 -0400

    Added fahrenheit to celsius fxn based off equation

--- CtoFTester.java
+++ CtoFTester.java
@@ -2,6 +2,9 @@ public class CtoFTester {
   public static double celsiusToFahrenheit(double c) {
     return (1.8 * c) + 32;
   }
+  public static double fahrenheitToCelsius(double f) {
+    return (f - 32) * 5.0/9.0;
+  }
   public static void main(String[] args) {
     System.out.println(20.0 + " degrees Celsius is " +
                 celsiusToFahrenheit(20.0) + " degrees Fahrenheit (68)");
